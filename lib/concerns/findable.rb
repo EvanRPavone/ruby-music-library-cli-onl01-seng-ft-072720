4cbc306e22f5c5a597c909e8ed9acd28ececc01b
Evan Pavone <evanrpavone@gmail.com>
2020-08-13 15:06:46 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,4 +4,14 @@ module Concerns::Findable
       nm.name == label
     end
   end
+
+  def find_or_create_by_name(label)
+    object = find_by_name(label)
+    if object == nil
+      create(label)
+    else
+      object
+    end
+  end
+
 end
